b4abdeb3c3b00eb41b31dcd6ea79bb8daee5ee63
jweboy <jweboy0630@gmail.com>
2025-12-28 21:40:55 +0800

    fix: 打包报错问题

--- components/web3Provider.tsx
+++ components/web3Provider.tsx
@@ -4,21 +4,20 @@ import { WagmiProvider, createConfig, http } from "wagmi";
 import { polygonAmoy } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
-//0x1d586bbc7147de00047261c1cc189d3b00ccdd1f
-//0x1d586bbc7147de00047261c1cc189d3b00ccdd1f
+import { injected, metaMask } from "wagmi/connectors";
 
+// 使用 ConnectKit 提供的助手函数，但显式声明以避免自动导入所有钱包
 const config = createConfig(
   getDefaultConfig({
-    // 你的应用名称
-    appName: "RWA Demo App",
-    // 配置支持的链，这里只用 Amoy
+    // 基础配置
+    appName: "RVI App",
     chains: [polygonAmoy],
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    connectors: [metaMask(), injected()],
+
     transports: {
-      // 使用默认的 RPC 节点，生产环境建议用 Alchemy/Infura
       [polygonAmoy.id]: http(process.env.NEXT_PUBLIC_POLYGON_RPC_URL),
     },
-    walletConnectProjectId:
-      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "", // 可选，去 WalletConnect 申请
   })
 );
 
